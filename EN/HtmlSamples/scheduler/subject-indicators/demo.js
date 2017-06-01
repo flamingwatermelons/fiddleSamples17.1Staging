5c0b8612c72913940d45d86031c252833f20103d
unknown <OctopusStaging@S0106WEB3S.IGWEB.LOCAL>
2017-06-01 10:23:57 -0400

    SB build 17.1.20171.32

--- EN/HtmlSamples/scheduler/subject-indicators/demo.js
+++ EN/HtmlSamples/scheduler/subject-indicators/demo.js
@@ -2,26 +2,25 @@ $(function () {
 $.ig.loader({
             scriptPath: "http://staging.igniteui.local/17-1/IgniteUI/js/",
             cssPath: "http://staging.igniteui.local/17-1/IgniteUI/css/",
-            resources: "igScheduler"
-        });
+            resources: "igScheduler",
+            ready: function() {
+                var scheduleListDataSource = new $.ig.scheduler.ScheduleListDataSource(),
+                    appointmentsDS = new $.ig.DataSource({
+                        primaryKey: "id",
+                        dataSource: appointments
+                    });
 
-        $.ig.loader(function () {
-            var scheduleListDataSource = new $.ig.scheduler.ScheduleListDataSource(),
-                appointmentsDS = new $.ig.DataSource({
-                    primaryKey: "id",
-                    dataSource: appointments
+                appointmentsDS.dataBind();
+
+                scheduleListDataSource.resourceItemsSource(resources);
+                scheduleListDataSource.appointmentItemsSource(appointmentsDS);
+
+                $("#scheduler").igScheduler({
+                    height: "650px",
+                    width: "100%",
+                    selectedDate: today,
+                    dataSource: scheduleListDataSource
                 });
-
-            appointmentsDS.dataBind();
-
-            scheduleListDataSource.resourceItemsSource(resources);
-            scheduleListDataSource.appointmentItemsSource(appointmentsDS);
-
-            $("#scheduler").igScheduler({
-                height: "650px",
-                width: "100%",
-                selectedDate: today,
-                dataSource: scheduleListDataSource
-            });
+            }
         });
 });
