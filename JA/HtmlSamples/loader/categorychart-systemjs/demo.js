a98140d55d459806c98641ea1bbfc2ba6c9a81c5
unknown <OctopusStaging@S0106WEB3S.IGWEB.LOCAL>
2017-06-01 09:37:58 -0400

    SB build 17.1.20171.31-a

---
+++ JA/HtmlSamples/loader/categorychart-systemjs/demo.js
@@ -0,0 +1,3 @@
+$(function () {
+System.import("http://jp.staging.igniteui.local/17-1/HtmlSamples/loader/categorychart-systemjs.js");
+});
---
+++ JA/HtmlSamples/loader/datachart-systemjs/demo.js
@@ -0,0 +1,3 @@
+$(function () {
+System.import("http://jp.staging.igniteui.local/17-1/HtmlSamples/loader/datachart-systemjs.js");
+});
