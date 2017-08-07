ad410c77a5c33dfd45d0876f17e4a35fadb005b3
unknown <OctopusStaging@S0106WEB3S.IGWEB.LOCAL>
2017-08-07 09:22:21 -0400

    SB build 17.1.20171.52

--- JA/HtmlSamples/scheduler/agenda-view/demo.js
+++ JA/HtmlSamples/scheduler/agenda-view/demo.js
@@ -1,30 +1,23 @@
-$(function () {
-$.ig.loader({
-            scriptPath: "http://staging.igniteui.local/17-1/IgniteUI/js/",
-            cssPath: "http://staging.igniteui.local/17-1/IgniteUI/css/",
-            resources: "igScheduler",
-            ready: function () {
+$( function () {
             var scheduleListDataSource = new $.ig.scheduler.ScheduleListDataSource(),
                 appointmentsDS = new $.ig.DataSource({
                     primaryKey: "id",
                     dataSource: appointments
                 });
 
-                appointmentsDS.dataBind();
+			appointmentsDS.dataBind();
 
-                scheduleListDataSource.resourceItemsSource(resources);
-                scheduleListDataSource.appointmentItemsSource(appointmentsDS);
+			scheduleListDataSource.resourceItemsSource(resources);
+			scheduleListDataSource.appointmentItemsSource(appointmentsDS);
 
-                $("#scheduler").igScheduler({
-                    height: "650px",
-                    width: "100%",
-                    agendaViewSettings: {
-                        dateRangeInterval: 10
-                    },
-                    views: ["agendaView"],
-                    selectedDate: today,
-                    dataSource: scheduleListDataSource
-                });
-            }
-        });
-});
+			$("#scheduler").igScheduler({
+				height: "650px",
+				width: "100%",
+				agendaViewSettings: {
+					dateRangeInterval: 10
+				},
+				views: ["agendaView"],
+				selectedDate: today,
+				dataSource: scheduleListDataSource
+			});
+		});
--- JA/HtmlSamples/scheduler/subject-indicators/demo.js
+++ JA/HtmlSamples/scheduler/subject-indicators/demo.js
@@ -1,26 +1,19 @@
-$(function () {
-$.ig.loader({
-            scriptPath: "http://staging.igniteui.local/17-1/IgniteUI/js/",
-            cssPath: "http://staging.igniteui.local/17-1/IgniteUI/css/",
-            resources: "igScheduler",
-            ready: function() {
-                var scheduleListDataSource = new $.ig.scheduler.ScheduleListDataSource(),
-                    appointmentsDS = new $.ig.DataSource({
-                        primaryKey: "id",
-                        dataSource: appointments
-                    });
-
-                appointmentsDS.dataBind();
-
-                scheduleListDataSource.resourceItemsSource(resources);
-                scheduleListDataSource.appointmentItemsSource(appointmentsDS);
-
-                $("#scheduler").igScheduler({
-                    height: "650px",
-                    width: "100%",
-                    selectedDate: today,
-                    dataSource: scheduleListDataSource
+$( function () {
+            var scheduleListDataSource = new $.ig.scheduler.ScheduleListDataSource(),
+                appointmentsDS = new $.ig.DataSource({
+                    primaryKey: "id",
+                    dataSource: appointments
                 });
-            }
+
+            appointmentsDS.dataBind();
+
+            scheduleListDataSource.resourceItemsSource(resources);
+            scheduleListDataSource.appointmentItemsSource(appointmentsDS);
+
+            $("#scheduler").igScheduler({
+                height: "650px",
+                width: "100%",
+                selectedDate: today,
+                dataSource: scheduleListDataSource
+            });
         });
-});
